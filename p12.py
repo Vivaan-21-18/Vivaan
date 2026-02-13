6366f4d737dbff2cdd9d6d9f6a37a8b45ebdd76d
Vivaan-21-18 <vivaan_shah_31242@pgkmschool.org>
2026-02-13 14:12:45 +0530

    Add files via upload

---
+++ p12.py
@@ -0,0 +1,52 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 22/7
+3.142857142857143
+>>> print("hello world")
+hello world
+>>> if 5>2:
+...     print("five is greater than two")
+... 
+...     
+five is greater than two
+>>> 
+========================================================================================== RESTART: C:/Users/Student/Desktop/Vivaan html/p2.py =========================================================================================
+five is greater than two
+five is greater than two
+five is greater than two
+>>> 
+================================================================================ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python314/p4.py ===============================================================================
+Five is greater than two
+Five is greater than two
+Five is greater than two
+>>> 
+================================================================================ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python314/p5.py ===============================================================================
+>>> 
+================================================================================ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python314/p6.py ===============================================================================
+Python is fun
+>>> 
+================================================================================ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python314/p7.py ===============================================================================
+Hello World
+Have a good day
+Learning Python is fun
+>>> 
+================================================================================ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python314/p8.py ===============================================================================
+Hello
+How are you?
+Bye Bye
+>>> 
+================================================================================ RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python314/p9.py ===============================================================================
+3
+358
+50000
+3.142857142857143
+>>> 
+=============================================================================== RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python314/p10.py ===============================================================================
+6
+10
+>>> 
+=============================================================================== RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python314/p11.py ===============================================================================
+I am 35 years old,
+>>> 
+=============================================================================== RESTART: C:/Users/Student/AppData/Local/Programs/Python/Python314/p11.py ===============================================================================
+I am 35 years old
